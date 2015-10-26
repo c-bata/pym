fae7bdeb98f247d8d752dd3feff90e639ff587a5
c-bata <contact@c-bata.link>
2015-10-26 19:23:13 +0900

    Assert pymanage install

--- test_pymanage.sh
+++ test_pymanage.sh
@@ -1,13 +1,32 @@
+#!/bin/sh
+
+chmod +x ./pymanage
+
+assert_exists_dir() {
+    if [ ! -d $1 ]; then
+        exit 1
+    fi
+}
+
+assert_exists_file() {
+    if [ ! -f $1 ]; then
+        exit 1
+    fi
+}
+
 case ${TRAVIS_OS_NAME} in
     "linux")
+        sudo ./pymanage install 3.5.0
+        assert_exists_dir '/opt/python-3.5.0'
         exit 0
         ;;
     "osx")
+        sudo ./pymanage install 3.5.0
+        assert_exists_dir '/opt/python-3.5.0'
         exit 0
         ;;
     *)
         exit 1
         ;;
 esac
-
 
